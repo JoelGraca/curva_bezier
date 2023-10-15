0162e87b7d9963b12df616e7d21e970df4ff089a
Joel da Graça <142855289+JoelGraca@users.noreply.github.com>
2023-10-15 12:01:25 +0000

    Curva_Bezier

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.CurvaBezier_CGI;
+
+public class Main {
+	// Testando a curva de Bezier
+	public static void main(String[] args) {
+		CurvaBezier curvaBezier = new CurvaBezier();
+		curvaBezier.setVisible(true);
+	}
+
+}
